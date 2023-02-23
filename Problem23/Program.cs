f5900862bc457d2c279ed3961c84ce219647904a
azamat Khadikov <khadikovazamat@gmail.com>
2023-02-23 10:02:10 -0700

    Problem23

---
+++ Problem23/Program.cs
@@ -0,0 +1,26 @@
+﻿int cube = 5;
+void Cube(int[] cube)
+{
+    int count = 0;
+    int length = cube.Length;
+    while (count < length)
+    {
+        cube[count] = Convert.ToInt32(Math.Pow(count, 3));
+        count++;
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int index = 0;
+    while (index < count)
+    {
+        Console.Write(coll[index] + " ");
+        index++;
+    }
+}
+
+int[] arry = new int[cube + 1];
+Cube(arry);
+PrintArray(arry);
